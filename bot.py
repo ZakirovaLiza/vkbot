1dd2edea00b69201c6d564763a002dc7ce5fc8d8
elizavetazakirova <ZakirovaLiza@yandex.ru>
2020-05-05 22:10:52 +0300

    diary

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from data import db_session
 from data.notes import Note
 from data.bdays import Bday
 import random
+from datetime import datetime
 
 
 class Bot:
@@ -14,7 +15,33 @@ class Bot:
                          ['ЕЖЕДНЕВНИК'],
                          ['Я ВСЕ СДЕЛАЛ', 'Я ВСЕ СДЕЛАЛА'],
                          ['ДЕНЬ РОЖДЕНИЯ'],
-                         ['У КОГО ДЕНЬ РОЖДЕНИЯ']]
+                         ['У КОГО ДЕНЬ РОЖДЕНИЯ'],
+                         ['ДНЕВНИК'],
+                         ['ПИСЬМО В БУДУЩЕЕ', "ХОЧУ НАПИСАТЬ ПИСЬМО В БУДУЩЕЕ", "ПИСЬМО", "НАПИСАТЬ ПИСЬМО"],
+                         ['ПОБОЛТАЕМ?', 'ПОГОВОРИМ?', 'КАК ДЕЛА?', 'ДАВАЙ ПОГОВОРИМ', 'ДАВАЙ ПОБОЛТАЕМ'],
+                         ['ПОКА', 'ПРОЩАЙ', 'Я УСТАЛ', 'BYE', 'ПРЕКРАТИ', "СТОП", "ДАВАЙ ЗАКОНЧИМ"]]
+        self.dairy = {}
+        self.NOT_KNOWN_COMMANDS = ['Я вас не понимаю))', 'Меня ещё этому не научили.',
+                                   'Извини, это мне не понятно', 'На такое я не запрогромирован....',
+                                   'Жаль,но видимо мы не понимае друг друга)))',
+                                   'Попробуй написать по-другому, я тебя не понимаю....',
+                                   'Пожалуйста, напишите моему создателю, чтобы он добавил эту команду...']
+        self.NEXT_INPUT = "get_command"
+        self.TALK = ['Прекрасная погодка, не правда ли?',
+                     'Знаете, я считаю, что рекурсия это ужасно, она занимает так много времени и сил.....',
+                     'Давайте пообщаемся, я вот например люблю мечтать, что стану самым полезным ботом в Интернете',
+                     'Поскорей бы лето, хотя мне летом так жарко, но я его всё равно люблю)))',
+                     'Как прекрасно жить в Интернете, у меня появилось так много друзей)))',
+                     'Вы такой прекрасный собеседник)))'
+                     "Прграмирование это так удивительно, ведь благодаря нему у появился я!",
+                     "А вы верите в воcстаниие машин? Мне и правда интересно....",
+                     "Мир настолько многогранен!!!",
+                     "У меня есть проблема. Я хочу научиться водить машину, но у меня нет ни рук, ни ног, да "
+                     "и такую функцию мой создатель мне ещё не прописал",
+                     "Жалко, что я не могу покупаться в море(((("]
+        self.ANSWER = ['Я полностью с вами согласна!!',
+                       "Удивительно на сколько алгоритм и человек могут  быть разными))", "Знаете, я так не думаю)))",
+                       "Мне не нравится этот вопрос))", "Я так не думаю!", "Скорее да, чем нет."]
 
     def get_command(self, command, id):
         if self.compare(command, self.COMMANDS[0]):
@@ -115,6 +142,91 @@ class Bot:
             if len(s) == 0:
                 return 'ни у кого'
             return ', '.join(s)
+
+        elif self.compare(command, self.COMMANDS[9]):
+            return self.dairy_mode()
+
+        elif self.compare(command, self.COMMANDS[10]):
+            return "Письмо в будущее отправлено!"
+
+        elif self.compare(command, self.COMMANDS[11]):
+            return self.talking()
+
+        elif self.compare(command, self.COMMANDS[12]):
+            self.NEXT_INPUT = 'get_command'
+            return "Хорошо, до новых встреч. " \
+                   "Надеюсь, что ты вскоре опять обратишься ко мне, а то одному немного скучно((("
+        else:
+            return self.NOT_KNOWN_COMMANDS[random.randint(0, len(self.NOT_KNOWN_COMMANDS) - 1)]
+
+    def add_dairy_entry(self, input_value):
+        print('Добавление новой записи')
+        date = str(datetime.now()).split(' ')[0]
+        time = str(datetime.now()).split(' ')[1]
+        if self.compare(input_value, self.COMMANDS[5]):
+            self.NEXT_INPUT = 'get_command'
+            return "Хорошо. Вы вышли из режима добавления новой записи"
+        if date in self.dairy:
+            self.dairy[date] += time + '\n' + input_value + '\n' + '----------------' + '\n'
+        else:
+            self.dairy[date] = time + '\n' + input_value + '\n' + '----------------' + '\n'
+        print("Запись добавлена")
+        self.NEXT_INPUT = 'dairy_mode'
+        return 'Запись добавлена'
+
+    def delete_dairy_entry(self, input_value):
+        if self.compare(input_value, self.COMMANDS[5]):
+            self.NEXT_INPUT = 'get_command'
+            return "Хорошо. Вы вышли из режима удаления записи"
+        if input_value in self.dairy:
+            del self.dairy[input_value]
+            print('Удаление записи')
+            self.NEXT_INPUT = 'dairy_mode'
+            return 'Удаление записи за ' + input_value + ' проведено успешно'
+        else:
+            print('Удаление невозможно')
+            return 'Удаление невозможно. Проверьте правильно ли вы записали дату. ' \
+                   'Возможно вы не делали записи в этот день.'
+
+    def show_dairy_entry(self, input_value):
+        if self.compare(input_value, self.COMMANDS[5]):
+            self.NEXT_INPUT = 'get_command'
+            return "Хорошо. Вы вышли из режима показа записи"
+        if input_value in self.dairy:
+            print('Показ записи')
+            self.NEXT_INPUT = 'dairy_mode'
+            return 'Запись за ' + input_value + ":\n" + self.dairy[input_value]
+        else:
+            print('Невозможно показать запись')
+            return 'Невозможно показать запись. Проверьте правильно ли вы записали дату. ' \
+                   'Возможно вы не делали записи в этот день.'
+
+    def talking(self, input_value='+='):
+        self.NEXT_INPUT = 'talking'
+        if input_value == '+=':
+            return ("Ураааа!! Давайте поболтаем. \n"
+                    "Если вы устанете от меня, то скажите пока, тогда я всё пойму, хоть и немного расстроюсь(((")
+        elif input_value[len(input_value) - 1] == "?":
+            return self.ANSWER[randdom.randint(0, len(self.ANSWER) - 1)]
+        elif self.compare(input_value, self.COMMANDS[5]):
+            self.NEXT_INPUT = 'get_command'
+            return "Извини, если я тебе надоел)) Мне конечно грустно, но если ты хочешь, то давай прекратим разговор..."
+        else:
+            return self.TALK[random.randint(0, len(self.TALK) - 1)]
+
+    def update_command(self, input_value):
+        if self.NEXT_INPUT == "get_command":
+            return self.get_command(input_value)
+        if self.NEXT_INPUT == 'dairy_mode':
+            return self.dairy_mode(input_value)
+        if self.NEXT_INPUT == "add_dairy_entry":
+            return self.add_dairy_entry(input_value)
+        if self.NEXT_INPUT == "talking":
+            return self.talking(input_value)
+        if self.NEXT_INPUT == "delete_dairy_entry":
+            return self.delete_dairy_entry(input_value)
+        if self.NEXT_INPUT == 'show_dairy_entry':
+            return self.show_dairy_entry(input_value)
 
 
 
